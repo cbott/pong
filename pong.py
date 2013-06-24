7ff1701539db7b0af1c4b971fed9cd43eb03443f
Colin <cbott6@gmail.com>
2013-06-24 15:24:56 -0400

    parent class and screen dimensions

--- pong.py
+++ pong.py
@@ -1,3 +1,13 @@
-import pygame
-print ("hi")
-pong=1
+#game of pong
+#first time i'm using git hub
+from livewires import games, color
+import random
+
+games.init(screen_width = 850, screen_height = 550, fps=45)
+
+class Thing(games.Sprite):
+    """parent class for ball and paddles"""
+    if self.top>games.screen.height:
+        self.top=games.screen.height
+    if self.bottom<0:
+        self.bottom=0       
