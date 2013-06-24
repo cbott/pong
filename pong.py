d2573f882c637b899387a88adccbf125d50a9675
Colin <cbott6@gmail.com>
2013-06-24 18:30:27 -0400

    added the ball, still doesnt rebound off of players paddle

--- pong.py
+++ pong.py
@@ -13,6 +13,30 @@ class Thing(games.Sprite):
         if self.bottom>games.screen.height:
             self.bottom=games.screen.height       
 
+class Ball(Thing):
+    image = games.load_image("ball.png")
+    SPEED = 3#speed multiplier
+
+    def __init__(self, x=games.screen.width/2, y=games.screen.height/2):
+        """create the ball"""
+        super(Ball, self).__init__(
+            image = Ball.image,
+            x=x, y=y,
+            dx = random.random() * Ball.SPEED,
+            dy=random.random() * Ball.SPEED)
+
+    def update(self):
+        super(Ball, self).update()
+        if self.top<=0:
+            #hit the top, reverse y direction
+            self.dy = -self.dy
+        if self.bottom>=games.screen.height:
+            #hit bottom, reverse y direction
+            self.dy = -self.dy
+        
+        
+    
+
 class Paddle(Thing):
     """player or computer paddle"""
     SPEED=2#how fast paddle moves up and down
@@ -58,6 +82,10 @@ class Game(object):
         #create the player's paddle
         self.player1 = Player(game = self,x=games.screen.width - 20)
         games.screen.add(self.player1)
+
+        #create the ball
+        self.ball = Ball()
+        games.screen.add(self.ball)
 
     def play(self):
         """play the game"""
