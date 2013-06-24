57d93965a53c63f730a929c1efc01a0932fcaf97
Colin <cbott6@gmail.com>
2013-06-24 16:20:12 -0400

    added music and general game things

--- pong.py
+++ pong.py
@@ -14,4 +14,28 @@ class Thing(games.Sprite):
 
 class Player(Thing):
     """paddle for player 1"""
-    image = games.load_image(
+    image = games.load_image("player1.png")
+
+
+def Game(object):
+    """the game"""
+
+    def play(self):
+        """play the game"""
+        #background music
+        games.music.load("theme_music.mid")
+        games.music.play(-1)
+
+        #set background image
+        background_image=games.load_image("background.png")
+        games.screen.background = background_image
+
+        #begin
+        games.screen.mainloop()
+
+def main():
+    pong = Game()
+    pong.play()
+
+#run the game
+main()
